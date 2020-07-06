a5f9fd056e55049d3fd330ff5224bd40314dca93
Dinuka Karunananda <dinuka.nirosh@gmail.com>
2020-07-06 12:14:35 +0530

    json url check

--- ImageFill.js
+++ ImageFill.js
@@ -1,12 +1,12 @@
 class ImageFill {
   static fill(grabfrom) {
-    if (grabfrom == undefined || grabfrom == null) return;
+    if (grabfrom == undefined || grabfrom == null || 0 === str.length) return;
     fetch(grabfrom).then(function (response) {
       if (response.status === 200) {
         response.json().then(function (data) {
           document.querySelectorAll("img[data-imagefill").forEach((img) => {
             let temp = data[img.dataset.imagefill];
-            if (temp == undefined || temp == null || temp.length == 0) return;
+            if (temp == undefined || temp == null || 0 === temp.length) return;
             img.src = temp;
           });
         });
